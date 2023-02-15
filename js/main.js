4c7a3ca6ea76a6f799e33537e6342a74c9d8201f
Kamilkwk12 <kamil.kwiatkowski2684@gmail.com>
2023-02-15 21:30:06 +0100

    repaired nav link, toggler button, and improved sets section

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const returnBtn = document.querySelector(".return-top");
 document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", function () {
         if (this.scrollY.valueOf() >= 250) {
-            returnBtn.classList.add("show");
+            returnBtn.classList.add("show-return-button");
         } else {
-            returnBtn.classList.remove("show");
+            returnBtn.classList.remove("show-return-button");
         }
     });
 });
